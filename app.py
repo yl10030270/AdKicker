46144afd845e89a3e7243e541278de938ec386fb
Leon <leon@Leons-MacBook-Pro.local>
2019-09-16 22:09:22 -0700

    changed link

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
+import requests
+from lxml import html
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    r = requests.get('https://www.luoxia.com/dazhuzai/')
+    dom = html.fromstring(r.text)
+    links = dom.xpath("//a[@href]")
+    for link in links:
+        link.attrib['href'] = '/page'
+    return html.tostring(dom, pretty_print=True)
+
+@app.route('/page')
+def page():    
+    return 'hello, world!'
