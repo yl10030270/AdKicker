1050a312c7e96a7961116af6431e2355fa3a9628
Leon <leon@Leons-MacBook-Pro.local>
2019-09-18 01:16:34 -0700

    functional version

--- app.py
+++ app.py
@@ -1,18 +1,60 @@
 import requests
 from lxml import html
+from lxml.html.clean import Cleaner
+from lxml.html import builder as E
+import re
+import logging
 
 from flask import Flask
 app = Flask(__name__)
 
+target_base_url = 'https://www.luoxia.com/dazhuzai/'
+cookies = {}
+
 @app.route('/')
 def index():
-    r = requests.get('https://www.luoxia.com/dazhuzai/')
-    dom = html.fromstring(r.text)
-    links = dom.xpath("//a[@href]")
-    for link in links:
-        link.attrib['href'] = '/page'
-    return html.tostring(dom, pretty_print=True)
+    r = requests.get(target_base_url)
+    cookies.update(r.cookies.items())
+    dom = cleanads(html.fromstring(r.text))
+    return html.tostring(pagify(dom), pretty_print=True)
 
-@app.route('/page')
-def page():    
-    return 'hello, world!'
+@app.route('/page/<id>')
+def page(id):
+    headers = {}
+    headers['User-Agent'] = 'PostmanRuntime/7.17.1'
+    headers['Host'] = 'www.luoxia.com'
+    headers['Cache-Control'] = 'no-cache'
+    url = target_base_url + id + '.htm'
+    print(cookies)
+    r = requests.get(url, headers=headers, cookies=cookies)
+    print(r.status_code)
+    print(r.request.headers)
+    dom = cleanads(html.fromstring(r.text))
+    return html.tostring(pagify(dom), pretty_print=True)
+
+def pagify(dom):
+    search_pattern = target_base_url + '(?P<id>\d+).htm';
+    a_links = dom.xpath("//a[@href]")
+    for link in a_links:
+        id = re.search(search_pattern, link.attrib['href'])
+        if id is not None:
+            if 'target' in link.attrib:
+                link.attrib.pop('target')
+            link.attrib['href'] = '/page/' + str(id.group('id'))
+
+    b_links = dom.xpath("//b[@onclick]")
+    for link in b_links:
+        id = re.search(search_pattern, link.attrib['onclick'])
+        if id is not None:
+            new_href = '/page/' + str(id.group('id'))
+            new_tag = E.A(link.text_content(), title=link.attrib["title"], href=new_href)
+            parent_node = link.getparent()
+            parent_node.replace(link, new_tag)
+
+    return dom
+
+def cleanads(dom):
+    async_scripts = dom.xpath("//script[@async]")
+    for script_tag in async_scripts:
+        script_tag.getparent().remove(script_tag)
+    return dom
