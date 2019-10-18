82d11325b904291941b583a327aa8694b1f0dae5
Leon <leon@Leons-MacBook-Pro.local>
2019-10-17 23:29:27 -0700

    add index page

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, redirect, request, abort
+from flask import Flask, redirect, request, abort, render_template
 from blueprint import zhongjidouluo_bp, mh1359_bp
 from urllib.parse import urljoin 
 
 app = Flask(__name__)
 app.register_blueprint(mh1359_bp, url_prefix='/' + mh1359_bp.name)
 app.register_blueprint(zhongjidouluo_bp, url_prefix='/' + zhongjidouluo_bp.name)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
